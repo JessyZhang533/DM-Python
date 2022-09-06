e9f74f1f88870d6070b9290237bf947a0a01c0d6
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-06 19:38:41 +0100

    basics 2nd commit

--- basics.py
+++ basics.py
@@ -16,3 +16,17 @@ print(not 20 < 3)
 print(4 / 2)  # Float division; result must be a float
 print(5 // 2)  # Integer division; result must be an integer
 print(5 % 2)  # Modulo; result must be an integer
+
+# Dictionary
+people_age = {"Anna": 18, "James": 7, "Lisa": 31}
+for i in people_age:  # Here i denotes the keys
+    print(i)
+for i, j in people_age.items():  # Here i denotes the keys, j denotes the values
+    print(i, j)
+
+# Enumerate https://www.programiz.com/python-programming/methods/built-in/enumerate
+my_list = [1, 3., 67]  # Also applicable to tuples
+print(type(enumerate(my_list)))  # This is an enumerate object
+print(list(enumerate(my_list)))  # Convert to a list to view
+
+# Differences between lists and tuples https://www.geeksforgeeks.org/python-difference-between-list-and-tuple/
