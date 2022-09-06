4dd846ec08d48f845f304b36270c3ff973b510e4
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-06 21:30:07 +0100

    numpy 2nd commit

--- libraries_np_ds.py
+++ libraries_np_ds.py
@@ -9,9 +9,9 @@ my_3d_array = np.array([[[1, 2, 3]], [[0, 9, 8]]])  # 3d array: need 3 layers of
 # print(my_2d_array)
 # print(my_3d_array)
 
-print(my_3d_array.ndim)  # '.ndim' gets the dimension of array
-print(my_3d_array.shape)  # get number of: 2d arrays in 3d arrays, 1d arrays in 2d arrays, elements in 1d arrays
-print(my_3d_array[1, 0, 0])  # indexing
+# print(my_3d_array.ndim)  # '.ndim' gets the dimension of array
+# print(my_3d_array.shape)  # get number of: 2d arrays in 3d arrays, 1d arrays in 2d arrays, elements in 1d arrays
+# print(my_3d_array[1, 0, 0])  # indexing
 
 # Use nested for loops to iterate over all elements
 a_3d_array = np.array([[[1, 2, 3], [-1, -2, -3]], [[4, 5, 6], [-4, -5, -6]]])
@@ -19,3 +19,12 @@ for i in a_3d_array:
     for j in i:
         for k in j:
             print(k)
+
+# Funcionalities
+# Create an array of zeros/ones
+zeros_array = np.zeros(10)
+print(zeros_array)  # The zeros are all floats
+print(zeros_array.astype(int))
+# Create an array prefilled with specific numbers
+prefilled_array = np.full(10, 5)  # An array of 10 elements, each element = 5
+print(prefilled_array)
