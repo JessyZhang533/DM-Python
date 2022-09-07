5e501d1386729a81cd5e89695a80b070b28082ec
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-07 17:05:59 +0100

    Scalar operations of np arrays

--- libraries_np_ds.py
+++ libraries_np_ds.py
@@ -9,9 +9,11 @@ my_3d_array = np.array([[[1, 2, 3]], [[0, 9, 8]]])  # 3d array: need 3 layers of
 # print(my_2d_array)
 # print(my_3d_array)
 
+
 # print(my_3d_array.ndim)  # '.ndim' gets the dimension of array
 # print(my_3d_array.shape)  # get number of: 2d arrays in 3d arrays, 1d arrays in 2d arrays, elements in 1d arrays
 # print(my_3d_array[1, 0, 0])  # indexing
+
 
 # Use nested for loops to iterate over all elements
 a_3d_array = np.array([[[1, 2, 3], [-1, -2, -3]], [[4, 5, 6], [-4, -5, -6]]])
@@ -20,11 +22,26 @@ for i in a_3d_array:
         for k in j:
             print(k)
 
+
 # Funcionalities
 # Create an array of zeros/ones
 zeros_array = np.zeros(10)
-print(zeros_array)  # The zeros are all floats
-print(zeros_array.astype(int))
+# print(zeros_array)  # The zeros are all floats
+# print(zeros_array.astype(int))
 # Create an array prefilled with specific numbers
 prefilled_array = np.full(10, 5)  # An array of 10 elements, each element = 5
-print(prefilled_array)
+# print(prefilled_array)
+
+
+# Scalar Operations
+# Addition & subtraction (CANNOT be performed on lists)
+print("Addition: {} \n Subtraction: {}".format(my_2d_array + 2, my_2d_array - 2))
+# Multiplication
+my_2d_list = [[1, 2, 3], [2, 3, 4], [7, 8, 9]]
+print("Multiplication of np arrays:\n{}".format(my_2d_array*2))  # All values of elements *2
+print("Multiplication of lists:\n{}".format(my_2d_list*2))  # Repeat all elements
+# Division (CANNOT be performed on lists)
+print("Float division:\n{}".format(my_2d_array/2))
+print("Integer division:\n{}".format(my_2d_array//2))
+# Raise to a power (CANNOT be performed on lists)
+print("\nRaise to a power:\n{}".format(my_2d_array**2))
