32f68c3982d1b746b46f7b27681c0b9e5a047ca1
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-08 18:49:01 +0100

    pandas filtering

--- Pandas_dataframe_series.py
+++ Pandas_dataframe_series.py
@@ -37,16 +37,31 @@ print(df.head(3))
 print(df.tail(3))
 # [n:m]: row slicing, n inclusive, m exclusive; Doesn't actually change df
 print(df[1:4])
+
+# INDEXING (Doesn't actually change df)
+# https://www.geeksforgeeks.org/select-rows-columns-by-name-or-index-in-pandas-dataframe-using-loc-iloc/#:~:text=Indexing%20in%20Pandas%20means%20selecting,also%20known%20as%20Subset%20selection.
+# 1.column indexing: single column: []; multiple columns: [[]]
+# 2.row indexing: single row: .loc[]; multiple rows: .loc[[]]
+print(df[['protein', 'rating']])
+print(df.loc[['Apple Jacks', 'All-Bran']])
+
+# FILTERING (Doesn't actually change df)
 # If we pass a list of boo lines to the brackets operator, we get another dataframe containing only
 # the rows for which the corresponding element in the list were true.
 second_row = [False, True, False, False, False, False, False, False, False, False]
 print(df[second_row])
-
-# INDEXING (Doesn't actually change df) https://www.geeksforgeeks.org/select-rows-columns-by-name-or-index-in-pandas-dataframe-using-loc-iloc/#:~:text=Indexing%20in%20Pandas%20means%20selecting,also%20known%20as%20Subset%20selection.
-# 1.column indexing: single column: []; multiple columns: [[]]
-# 2.row indexing: single row: .loc[]; multiple rows: .loc[[]]
-# print(df[['protein', 'rating']])
-print(df.loc[['Apple Jacks', 'All-Bran']])
+# Filtering rows: apply condiitons (essentially boolean lists)
+print(df[df['calories'] > 70])  # one condition
+print(df[(df['calories'] > 70) & (df['protein'] < 4)])  # group conditions: and, &
+print(df[(df['calories'] > 70) | (df['protein'] > 3)])  # group conditions: or, |
+# .loc[..., ...]: 1st argument-->row label, 2nd argument-->column label
+print(df.loc['All-Bran', 'protein'])
+# .loc[[...], [...]]: same as above but get a dataframe
+print(df.loc[['All-Bran'], ['protein']])
+# .loc[...:..., ...:...]: slice rows & columns
+print(df.loc['100% Bran':'All-Bran', 'calories':'protein'])
+# .loc[[..., ...], ...:...]: index rows, slice columns; vice versa
+print(df.loc[['100% Bran', 'All-Bran'], 'calories':'protein'])
 
 # .describe(): Get statistical summary of the dataframe https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.describe.html
 # print(df.describe())
