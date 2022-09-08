3d525bd67bd681f458f8bffca5581a551bc07a8e
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-09 00:37:53 +0100

    Dealing with missing values

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+from numpy import NaN
+from statistics import mode
 
 
 X = pd.Series([2.1, 2.2, 4.5, 2.2, 2.3])
 
 
-# Median based anomaly detection
 def median_based_anomaly_detection(series, threshold):  # Set a range/threshold
     " Range for normal data: |data - median| <= threshold "
     outliers = []
@@ -49,3 +50,16 @@ print(median_based_anomaly_detection(X, 2))
 print(mean_based_anomaly_detection(X))
 print(z_score_anomaly_detection(X, 1.5))
 print(interquatile_anomaly_detection(X))
+
+
+# Dealing with missing values
+df_1 = pd.DataFrame({'Name': ['James', 'Leo', 'Zoe', 'Astrid'], 'Age': [12, 12, NaN, 14]})  # In pandas, NaN means null
+# 1. .isnull() tells if a cell is empty
+print(df_1.isnull())
+# 2. .sinull() + .sum() finds the total number of missing values
+print(df_1.isnull().sum())
+# 3. .dropna(): deleting rows with missing value; inplace=True to actually change the dataframe
+print(df_1.dropna(inplace=False))
+# 4. .fillna(): replacing missing data with mean/median/mode; inplace=True to actually change the dataframe
+print(df_1.fillna(df_1.mean(), inplace=False))
+print(df_1.fillna(mode(df_1['Age']), inplace=False))
